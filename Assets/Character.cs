81be9418d150d2e79d2528aba0e9b0e35f099e39
BYUNSUJUNG <susususu07@naver.com>
2019-10-01 19:38:37 +0900

    git init 2019-10-01

---
+++ Assets/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public float moveSpeed = 2f;
+	public float jumpPower = 300f;
+
+	public GameManager gameManager;
+	//public GameObject gameManager;
+
+	void Update () {
+		// Uadate 프레임 시간 차 * Time.Timescale = Time.deltaTime
+		transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+	}
+
+	public void Jump() {
+		GetComponent<Rigidbody2D>().AddForce(Vector3.up*jumpPower);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.transform.name == "WinCollider") {
+			gameManager.Win();
+		} else if (col.transform.name == "LoseCollider") {
+			gameManager.Lose();
+		}
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // replay()에서 사용됨
+
+public class GameManager : MonoBehaviour {
+	public int score;
+	public GameObject winPanelObj, losePanelObj;
+
+	void start() {
+		Time.timeScale = 1f;
+	}
+	public void Win() {
+		winPanelObj.SetActive(true);
+		Time.timeScale = 0f;
+//		Debug.Log("Win!");
+	}
+
+	public void Lose() {
+		losePanelObj.SetActive(true);
+		Time.timeScale = 0f;
+//		Debug.Log("Lose!");
+	}
+
+	public void Replay() {
+		SceneManager.LoadScene("Play");
+	}
+}
